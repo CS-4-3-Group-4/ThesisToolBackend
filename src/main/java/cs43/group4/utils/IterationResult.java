119fc8fba4fada757ccaf47f9cc838807e0f1a55
John Paul <johnpaulviado20@gmail.com>
2025-10-04 03:13:00 +0800

    feat: add IterationResult class to encapsulate iteration data

---
+++ src/main/java/cs43/group4/utils/IterationResult.java
@@ -0,0 +1,11 @@
+package cs43.group4.utils;
+
+public class IterationResult {
+    public int iteration;
+    public double fitness;
+
+    public IterationResult(int iteration, double fitness) {
+        this.iteration = iteration;
+        this.fitness = fitness;
+    }
+}
