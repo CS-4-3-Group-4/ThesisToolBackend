832ded82678f55a182e477b77bdde2a3f1af0b66
John Paul <johnpaulviado20@gmail.com>
2025-10-28 23:06:21 +0800

    feat: add ProfileResult class for memory and execution time profiling

---
+++ src/main/java/cs43/group4/utils/ProfileResult.java
@@ -0,0 +1,47 @@
+package cs43.group4.utils;
+
+public class ProfileResult {
+        private final long memoryUsedBytes;
+        private final long executionTimeNs;
+
+        public ProfileResult(long memoryUsedBytes, long executionTimeNs) {
+            this.memoryUsedBytes = memoryUsedBytes;
+            this.executionTimeNs = executionTimeNs;
+        }
+
+        // Memory getters
+        public long getMemoryUsedBytes() {
+            return memoryUsedBytes;
+        }
+
+        public double getMemoryUsedKB() {
+            return memoryUsedBytes / 1024.0;
+        }
+
+        public double getMemoryUsedMB() {
+            return memoryUsedBytes / (1024.0 * 1024.0);
+        }
+
+        // Time getters
+        public long getExecutionTimeNs() {
+            return executionTimeNs;
+        }
+
+        public double getExecutionTimeMs() {
+            return executionTimeNs / 1_000_000.0;
+        }
+
+        public double getExecutionTimeS() {
+            return executionTimeNs / 1_000_000_000.0;
+        }
+
+        @Override
+        public String toString() {
+            return String.format(
+                "Memory Used: %.2f MB (%.2f KB, %d bytes)\n" +
+                "Execution Time: %.3f s (%.3f ms, %d ns)",
+                getMemoryUsedMB(), getMemoryUsedKB(), memoryUsedBytes,
+                getExecutionTimeS(), getExecutionTimeMs(), executionTimeNs
+            );
+        }
+    }
